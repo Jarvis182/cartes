f92ecdbc5cea16e1a6e109bfc6e35607def100df
Jarvis182 <patrice.hanne@gmail.com>
2019-10-11 15:12:29 +0200

    add script + src

--- Scripts/deliver.bat
+++ Scripts/deliver.bat
@@ -1,4 +1,5 @@
 @echo OFF
+echo Salut Champion
 set ARTIFACT=superPoker.war
-set DESTDIR=C:\Users\User\3D Objects\Serveur\apache-tomcat-9.0.26\webapps
+set DESTDIR=C:\Users\User\Documents\SERVEUR_TEST\Serveur\apache-tomcat-9.0.26\webapps
 copy target\%ARTIFACT% %DESTDIR%
